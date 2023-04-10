dc577bdbcb2427206269a8bc550289141ce963d6
1ukefg <lukefletchr@gmail.com>
2023-04-10 16:20:34 +0000

    fix the events lsiteners for each cell and correct my mistake with the name of variables. Added text content to the boxes so that the user/s can enter their move onto the grid. Added a subsequent if statement that will check if there is a game winning move after every input on the grid, if there is, a winning message is displayed, if not, the game will continue and it will be the opposing players turn, again marked with some text to say so which makes it easy for any user to follow and understand what they are to do next and which point of  the game they are at.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,30 +10,44 @@ const X_WON = 'X_WON'
 const O_WON = 'O_WON'
 const DRAW = 'DRAW'
 
-boxes.forEach((box, index) => {
-    box.addEventListener("click", () => {
-        if (gameBoard[index] !== "" || gameWon) {
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', () => {
+        if (board[i] !== '' || !gameIsActive) {
             return;
-          }
-
-        function checkWin() {
-            const winConditions = [
-                [0, 1, 2],
-                [3, 4, 5],
-                [6, 7, 8],
-                [0, 3, 6],
-                [1, 4, 7],
-                [2, 5, 8],
-                [0, 4, 8],
-                [2, 4, 6]
-            ];
-
-            for (let i = 0; i < winConditions.length i++) {
-                let [a, b, c] = winConditions[i];
-                if (gameBoard[a] !== "" && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-                    return true;
-                }
-            }
-
-            return false;
         }
+
+        boxes[i].textContent = activePlayer;
+        board[i] = activePlayer;
+
+        if (checkWin()) {
+            gameIsActive = false;
+            info.textContent = `${activePlayer} Won!`;
+            return;
+        }
+
+        activePlayer = activePlayer === 'X' ? 'O' : 'X';
+        info.textContent = `${activePlayer}'s turn...`;
+    });
+}
+
+function checkWin() {
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winConditions.length; i++) {
+        let [a, b, c] = winConditions[i];
+        if (gameBoard[a] !== "" && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return true;
+        }
+    }
+
+    return false;
+}
