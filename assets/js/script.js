9f8cbf2c5df5fe8ecca6f05b84387a2d14e9df0e
1ukefg <lukefletchr@gmail.com>
2023-04-10 12:17:42 +0000

    create 3 key variables which will be used for the game. Board will act as the board, activePlayer will log the active player and gameIsActive will recognise whether a game finishing result has been input and if not to continue the game.I have added three string values to announce each type of end game event. the winConditions variable collects and logs all of the ways that a game is won.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log('We are in')
+const boxes = document.getElementsByClassName('box')
+const info = document.getElementsByClassName('info')
+const resetBtn = document.getElementById('reset')
+
+let board = ['', '', '', '', '', '', '', '', ''];
+let activePlayer = 'X';
+let gameIsActive = true;
+
+const X_WON = 'X_WON'
+const O_WON = 'O_WON'
+const DRAW = 'DRAW'
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
