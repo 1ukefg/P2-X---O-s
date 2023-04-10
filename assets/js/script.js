7e440b1d937f70ab180cd65a954160e7bea92147
1ukefg <lukefletchr@gmail.com>
2023-04-10 14:20:19 +0000

    create function that will check if there is a winner of the game, given that the current state of the game at any gicven point matches the win conditions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,24 @@ const X_WON = 'X_WON'
 const O_WON = 'O_WON'
 const DRAW = 'DRAW'
 
-const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+function checkWin() {
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
+    for (let i = 0; i < winConditions.length i++) {
+        let [a, b, c] = winConditions[i];
+        if (gameBoard[a] !== "" && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return true;
+        }
+    }
+
+    return false:
+}
