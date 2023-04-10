39dcbbdb0f3301e13011da00441bf2ebff2c3270
1ukefg <lukefletchr@gmail.com>
2023-04-10 14:43:21 +0000

    Add event listeners for each cell that a user can select on their turn. Write an if statement so that if a box is already marked or the game is over it will return.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,30 @@ const X_WON = 'X_WON'
 const O_WON = 'O_WON'
 const DRAW = 'DRAW'
 
-function checkWin() {
-    const winConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
+boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+        if (gameBoard[index] !== "" || gameWon) {
+            return;
+          }
 
-    for (let i = 0; i < winConditions.length i++) {
-        let [a, b, c] = winConditions[i];
-        if (gameBoard[a] !== "" && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-            return true;
+        function checkWin() {
+            const winConditions = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+
+            for (let i = 0; i < winConditions.length i++) {
+                let [a, b, c] = winConditions[i];
+                if (gameBoard[a] !== "" && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+                    return true;
+                }
+            }
+
+            return false;
         }
-    }
-
-    return false:
-}
